043df498ccbfdea91bab8986e0cbe67ac415f2ed
jayminkarthiya <karathiyajayaminp@gmail.com>
2026-03-11 20:01:40 +0530

    Ch 51 - HTML Raw - Practice

---
+++ ASPDOTNETMVC5ByWebGentle/Controllers/Ch51HTMLRawController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ASPDOTNETMVC5ByWebGentle.Controllers
+{
+    public class Ch51HTMLRawController : Controller
+    {
+        // GET: Ch51HTMLRaw
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASPDOTNETMVC5ByWebGentle/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ASPDOTNETMVC5ByWebGentle.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
